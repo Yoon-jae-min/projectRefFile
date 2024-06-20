9251acb4e7e5833e5ced36daa735f47eb085af01
Yoon-Jae-Min <jmyoon1994@naver.com>
2024-06-21 00:29:13 +0900

    20240621-recipeSQL-update

---
+++ LesChef(Zero Table 변형)/recipeData/sql파일/recipe테이블데이터/recipe-japanese.sql
@@ -0,0 +1,80 @@
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(1, "스키야키", 0, writeDate, 25, "2~3인분", "★★☆", null, 이미지, "일식", "국/전골", "https://wtable.co.kr/recipes/j1QYeUgj9y6zidwaNJD22NTg?location=search");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(2, "미소 시루", 0, writeDate, 30, "4~5인분", "★☆☆", null, 이미지, "일식", "국/전골", "https://wtable.co.kr/recipes/v25KBndcuuk4sdjGbfr8PUA2?location=recipe_home");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(3, "옥도미 맑은국", 0, writeDate, 80, "2~3인분", "★★☆", null, 이미지, "일식", "국/전골", "https://wtable.co.kr/recipes/E9KLkhjdNgCwFWQxHKvZnvXa?location=recipe_home");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(4, "어묵전골", 0, writeDate, 25, "2~3인분", "★★☆", null, 이미지, "일식", "국/전골", "https://wtable.co.kr/recipes/b5gvy3pyxt5ySWbNocWDph4V?location=recipe_home");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(5, "밀푀유나베", 0, writeDate, 45, "2~3인분", "★☆☆", null, 이미지, "일식", "국/전골", "https://wtable.co.kr/recipes/K9a135iyFtmccy1Tk7KCsuCe?location=search");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(6, "돈가스 김치나베", 0, writeDate, 30, "2인분", "★★☆"null, 이미지, "일식", "국/전골", "https://wtable.co.kr/recipes/EMSHygPSHMM3q6LZuuXAcwUY?location=search");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(7, "우엉 불고기나베", 0, writeDate, 20, "2인분", "★☆☆", null, 이미지, "일식", "국/전골", "https://wtable.co.kr/recipes/GYh4wxVyFGFXicBGjq8xvDWa?location=search");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(8, "야끼소바", 0, writeDate, 40, "3인분", "★☆☆", null, 이미지, "일식", "면", "https://wtable.co.kr/recipes/zRwd7VXL6Rp7aoSjpfkf4C9K?location=search");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(9, "붓카케우동", 0, writeDate, 15, "2인분", "★☆☆", null, 이미지, "일식", "면", "https://wtable.co.kr/recipes/99ET9LQ448Pv1pt9WYbaL3Nq?location=search");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(10, "된장 버터 옥수수라면", 0, writeDate, 15, "2인분", "★☆☆", null, 이미지, "일식", "면", "https://wtable.co.kr/recipes/ynRji9pg8McpHMchYigEj97E?location=recipe_home");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(11, "대파 닭고기우동", 0, writeDate, 20, "2인분", "★☆☆", null, 이미지, "일식", "면", "https://wtable.co.kr/recipes/ZQ1u19nBCyPAxjxhWULLiRWr?location=recipe_home");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(12, "야끼스파게티", 0, writeDate, 40, "2인분", "★★☆", null, 이미지, "일식", "면", "https://wtable.co.kr/recipes/jEE71yJ1WQmyUnbvxap3cz9P?location=recipe_home");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(13, "카레우동", 0, writeDate, 25, "2인분", "★★☆", null, 이미지, "일식", "면", "https://wtable.co.kr/recipes/FDPiDhXesyAcjDzmakACx4Xy?location=recipe_home");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(14, "새우튀김냉메밀", 0, writeDate, 50, "3인분", "★★☆", null, 이미지, "일식", "면", "https://wtable.co.kr/recipes/LjiJ7qCnJ3V4ZxdnWwk2nCGn?location=recipe_home");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(15, "나가사키짬뽕", 0, writeDate, 30, "2인분", "★★☆", null, 이미지, "일식", "면", "https://wtable.co.kr/recipes/RPhzfkAMZr3ASnCAPFLGGeDS?location=recipe_home");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(16, "톳 소바", 0, writeDate, 15, "1인분", "★☆☆", null, 이미지, "일식", "면", "https://wtable.co.kr/recipes/3avDjR7mc9d83wZvCvtfMzn9?location=search");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(17, "아보카도 비프 소바", 0, writeDate, 40, "1인분", "★☆☆", null, 이미지, "일식", "면", "https://wtable.co.kr/recipes/aesENwNJocioraChMSenfJVu?location=search");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(18, "참깨 메밀 소바", 0, writeDate, 20, "2인분", "★☆☆", null, 이미지, "일식", "면", "https://wtable.co.kr/recipes/rqUTt76YrJGdKTxZcLBJzman?location=search");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(19, "냉소바", 0, writeDate, 25, "1인분", "★☆☆", null, 이미지, "일식", "면", "https://wtable.co.kr/recipes/pFfmJx5NMhYifrZ2njJAh7f6?location=search");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(20, "연어 아보카도 덮밥", 0, writeDate, 20, "2인분", "★☆☆", null, 이미지, "일식", "밥", "https://wtable.co.kr/recipes/B2yYCMiL8iiJ1nQJqzqf17uQ?location=recipe_home");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(21, "고등어덮밥", 0, writeDate, 35, "2인분", "★★☆", null, 이미지, "일식", "밥", "https://wtable.co.kr/recipes/1p89E4q79WvuoGRKFZdzUVMv?location=recipe_home");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(22, "튀김덮밥(텐동)", 0, writeDate, 45, "2인분", "★★☆", null, 이미지, "일식", "밥", "https://wtable.co.kr/recipes/JDcKtRnopd3LKKkakvdZqRQP?location=recipe_home");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(23, "쇠고기덮밥", 0, writeDate, 20, "2인분", "★☆☆", null, 이미지, "일식", "밥", "https://wtable.co.kr/recipes/JzC4jiBFnF8Sor9NeJ9VNKpb?location=recipe_home");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(24, "장어덮밥", 0, writeDate, 20, "2인분", "★★☆", null, 이미지, "일식", "밥", "https://wtable.co.kr/recipes/p9DQF4cEGaUV9rxmXjkSgkVu?location=recipe_home");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(25, "연어오차즈케", 0, writeDate, 30, "1인분", "★☆☆", null, 이미지, "일식", "밥", "https://wtable.co.kr/recipes/gcPDXcjdTDKvPNWDK2urZC6x?location=recipe_home");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(26, "차슈덮밥", 0, writeDate, 40, "2인분", "★☆☆", null, 이미지, "일식", "밥", "https://wtable.co.kr/recipes/vtYQJHb6ujs2jy18LHenUgCy?location=recipe_home");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(27, "연어장과 연어덮밥", 0, writeDate, 40, "7~8인분", "★★☆", null, 이미지, "일식", "밥", "https://wtable.co.kr/recipes/1HGYZE7L4CV1FxFqLiDYzKnz?location=recipe_home");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(28, "차돌박이 초밥", 0, writeDate, 30, "2인분", "★★☆", null, 이미지, "일식", "밥", "https://wtable.co.kr/recipes/Ta5K8T18HnBLS6ChKRvnAm5Y?location=search");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(29, "전복 시소말이 튀김", 0, writeDate, 30, "1인분", "★☆☆", null, 이미지, "일식", "기타", "https://wtable.co.kr/recipes/VhwzqXD6kKegSD3MHC5jVH8c?location=recipe_home");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(30, "고등어 미소 조림", 0, writeDate, 40, "1~2인분", "★☆☆", null, 이미지, "일식", "기타", "https://wtable.co.kr/recipes/ik8VdNeXGH6GNUSbgZtwtcrN?location=recipe_home");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(31, "연어달걀찜", 0, writeDate, 30, "2~3인분", "★☆☆", null, 이미지, "일식", "기타", "https://wtable.co.kr/recipes/fwAphDQzbH8SXTpNcLGhdhqt?location=recipe_home");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(32, "타코야끼", 0, writeDate, 20, "2~3인분", "★★☆", null, 이미지, "일식", "기타", "https://wtable.co.kr/recipes/P2vQNRh6uhP2mc5bSe7WEbB9?location=recipe_home");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(33, "치즈돈가스", 0, writeDate, 30, "2인분", "★★☆", null, 이미지, "일식", "기타", "https://wtable.co.kr/recipes/aUGL7RjpWVuZikZksBAT3Van?location=recipe_home");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(34, "일본식 두부튀김", 0, writeDate, 20, "2인분", "★☆☆", null, 이미지, "일식", "기타", "https://wtable.co.kr/recipes/tD1NBr1ufXyi5B8u4AzdtcGm?location=recipe_home");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(35, "치킨가라아게", 0, writeDate, 45, "2인분", "★☆☆", null, 이미지, "일식", "기타", "https://wtable.co.kr/recipes/cJkmF5qPUDo79HbW4BcxSYjv?location=recipe_home");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(36, "새우카츠샌드", 0, writeDate, 35, "2개 분량", "★★☆", null, 이미지, "일식", "기타", "https://wtable.co.kr/recipes/5but9gmk2Eyt47BTHDUcNksZ?location=recipe_home");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(37, "참치타다키", 0, writeDate, 40, "2인분", "★★☆", null, 이미지, "일식", "기타", "https://wtable.co.kr/recipes/EWqDs9WBNe2VcVJMP8AwGLEz?location=recipe_home");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(38, "달래 오코노미야키", 0, writeDate, 30, "2장 분량", "★★☆", null, 이미지, "일식", "기타", "https://wtable.co.kr/recipes/TgJMdwLRN9DG3D6Vd71uLhhb?location=recipe_home");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(39, "야키도리", 0, writeDate, 40, "2인분", "★☆☆", null, 이미지, "일식", "기타", "https://wtable.co.kr/recipes/D46sHAPZuML6SDxVAeLSRJxV?location=recipe_home");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(40, "물오징어 빵꼬아게", 0, writeDate, 30, "1~2인분", "★☆☆", null, 이미지, "일식", "기타", "https://wtable.co.kr/recipes/z8t81CMj4Y2Gn6NsVvDG7QZu?location=recipe_home");
---
+++ LesChef(Zero Table 변형)/recipeData/sql파일/recipe테이블데이터/recipe-korean.sql
@@ -0,0 +1,80 @@
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(1, "성게 미역국", 0, writeDate, 30, "2인분", "★☆☆", null, 이미지, "한식", "국/찌개", "https://wtable.co.kr/recipes/aqnyvZDijf8Ui8o4GLFnjQGB?location=recipe_home");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(2, "곤드레 전", 0, writeDate, 20, "2인분", "★☆☆", null, 이미지, "한식", "기타", "https://wtable.co.kr/recipes/j7EELtk9r1nfhGShBvrtQR6X?location=recipe_home");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(3, "미역 오이냉국", 0, writeDate, 10, "2인분", "★☆☆", null, 이미지, "한식", "국/찌개", "https://wtable.co.kr/recipes/1sMUNTAgz4b8XvmPfcq7aQfs?location=recipe_home");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(4, "아욱 된장국", 0, writeDate, 20, "2인분", "★☆☆", null, 이미지, "한식", "국/찌개", "https://wtable.co.kr/recipes/4GW6AeEXGSfvzKR5YBd1TyZp?location=recipe_home");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(5, "목살 김치찜", 0, writeDate, 90, "3인분", "★☆☆", null, 이미지, "한식", "기타", "https://wtable.co.kr/recipes/QzEpZUQgqvrVD8raoiYVrATH?location=recipe_home");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(6, "김치 비빔국수", 0, writeDate, 25, "2인분", "★☆☆"null, 이미지, "한식", "밥/면", "https://wtable.co.kr/recipes/QDk9nnKsEerfspNHrRtT9Gpp?location=recipe_home");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(7, "꼬막장", 0, writeDate, 20, "3~4인분", "★☆☆", null, 이미지, "한식", "반찬", "https://wtable.co.kr/recipes/4SG6cGZYTCdsBvqafPgrxQ4a?location=recipe_home");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(8, "파채 불고기", 0, writeDate, 35, "2인분", "★☆☆", null, 이미지, "한식", "반찬", "https://wtable.co.kr/recipes/k3J7GP6EUg4FiAkPiRAWCEWu?location=recipe_home");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(9, "감자채볶음", 0, writeDate, 25, "4인분", "★☆☆", null, 이미지, "한식", "반찬", "https://wtable.co.kr/recipes/yPUXXu7eSf9ZN5bxrjGFAxGT?location=recipe_home");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(10, "쇠고기 미역국", 0, writeDate, 50, "4인분", "★☆☆", null, 이미지, "한식", "국/찌개", "https://wtable.co.kr/recipes/Yg6T2drYTBBbrbMRGccZ2XX6?location=recipe_home");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(11, "김치 순두부찌개", 0, writeDate, 35, "2인분", "★★☆", null, 이미지, "한식", "국/찌개", "https://wtable.co.kr/recipes/k4hiGGLQzRFEyCCFhi9YzMNk?location=search");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(12, "떡국", 0, writeDate, 25, "2인분", "★☆☆", null, 이미지, "한식", "국/찌개", "https://wtable.co.kr/recipes/74d6XEKwF3PNiPih914ycaPn?location=search");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(13, "황태국", 0, writeDate, 35, "2~3인분", "★★☆", null, 이미지, "한식", "국/찌개", "https://wtable.co.kr/recipes/VHWZjwytDBNQzL8LdkPnMZkg?location=search");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(14, "오징어뭇국", 0, writeDate, 30, "2인분", "★☆☆", null, 이미지, "한식", "국/찌개", "https://wtable.co.kr/recipes/3GY8t1JWVVAyL6ZnvCmiCVWb?location=search");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(15, "콩나물국", 0, writeDate, 20, "2인분", "★☆☆", null, 이미지, "한식", "국/찌개", "https://wtable.co.kr/recipes/3QSo2JFJSa7YoLTBzYULfLQQ?location=search");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(16, "참치김치찌개", 0, writeDate, 25, "2인분", "★☆☆", null, 이미지, "한식", "국/찌개", "https://wtable.co.kr/recipes/ynveZVxzRQECJPwJsLQabsiA?location=search");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(17, "표고버섯 덮밥", 0, writeDate, 30, "2인분", "★☆☆", null, 이미지, "한식", "밥/면", "https://wtable.co.kr/recipes/f2v1e5Kd4caoW1vZcwrsJG7v?location=recipe_home");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(18, "스팸마요 덮밥", 0, writeDate, 20, "1인분", "★☆☆", null, 이미지, "한식", "밥/면", "https://wtable.co.kr/recipes/86kXDnKtJ5a7goKPE7NwcJ6Z?location=search");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(19, "달걀밥", 0, writeDate, 15, "2인분", "★☆☆", null, 이미지, "한식", "밥/면", "https://wtable.co.kr/recipes/xNJeUtapRLiLUUdSgJR37QUa?location=search");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(20, "콩나물 쫄면", 0, writeDate, 15, "2인분", "★☆☆", null, 이미지, "한식", "밥/면", "https://wtable.co.kr/recipes/2rH1UjNHsFTffCTq7sLEUWHs?location=search");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(21, "장칼국수", 0, writeDate, 40, "2인분", "★★☆", null, 이미지, "한식", "밥/면", "https://wtable.co.kr/recipes/B1RTqw2YwgEUhZ3YjzMJgQWc?location=recipe_home");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(22, "달걀김밥", 0, writeDate, 30, "3줄", "★★☆", null, 이미지, "한식", "밥/면", "https://wtable.co.kr/recipes/PVFeTmytZkN9CxhEXQw3X8wn?location=search");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(23, "깍두기볶음밥", 0, writeDate, 20, "2인분", "★☆☆", null, 이미지, "한식", "밥/면", "https://wtable.co.kr/recipes/Tu138VDWTMTcqgzyD2NkxHiS?location=recipe_home");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(24, "간장비빔국수", 0, writeDate, 15, "2인분", "★☆☆", null, 이미지, "한식", "밥/면", "https://wtable.co.kr/recipes/TYrpZcRhHki3cXKAxW8v8jBt?location=recipe_home");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(25, "김치말이국수", 0, writeDate, 60, "2인분", "★☆☆", null, 이미지, "한식", "밥/면", "https://wtable.co.kr/recipes/nuKnwBSAbZJzswX23eMT1VWz?location=search");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(26, "오징어채볶음", 0, writeDate, 15, "4인분", "★☆☆", null, 이미지, "한식", "반찬", "https://wtable.co.kr/recipes/KrkYZZzwoWLEQ9s2VxbGx8zZ?location=search");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(27, "참치김치볶음", 0, writeDate, 30, "4인분", "★☆☆", null, 이미지, "한식", "반찬", "https://wtable.co.kr/recipes/4fe7fbAXyXWU2A5VBwpBVpx3?location=search");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(28, "청포묵무침", 0, writeDate, 10, "2~3인분", "★☆☆", null, 이미지, "한식", "반찬", "https://wtable.co.kr/recipes/CQjvWyUdk6Dyq2xZNPUgt3uX?location=search");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(29, "두부무조림", 0, writeDate, 35, "2인분", "★★☆", null, 이미지, "한식", "반찬", "https://wtable.co.kr/recipes/FdGRJzYLaVm3ZK5vQjn2bFkt?location=search");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(30, "부대볶음", 0, writeDate, 20, "2인분", "★☆☆", null, 이미지, "한식", "반찬", "https://wtable.co.kr/recipes/841RC5MgJtn6GXrHicGtZFm2?location=search");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(31, "명엽채볶음", 0, writeDate, 15, "4~5인분", "★☆☆", null, 이미지, "한식", "반찬", "https://wtable.co.kr/recipes/UdZ9DByWG4n8uWQtwgqU52sr?location=search");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(32, "삼겹살 간장조림", 0, writeDate, 120, "4인분", "★★☆", null, 이미지, "한식", "반찬", "https://wtable.co.kr/recipes/JBrJqVtJHfVzSgHPMoL2yjS1?location=search");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(33, "소떡소떡", 0, writeDate, 20, "4개 분량", "★☆☆", null, 이미지, "한식", "기타", "https://wtable.co.kr/recipes/ctjKfmaatD6a4T5hxJfoPsYG?location=search");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(34, "양배추 전", 0, writeDate, 20, "1~2인분", "★★☆", null, 이미지, "한식", "기타", "https://wtable.co.kr/recipes/hKB8JrarDFtBx938uAZyGLsz?location=search");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(35, "궁중떡볶이", 0, writeDate, 25, "2인분", "★★☆", null, 이미지, "한식", "기타", "https://wtable.co.kr/recipes/UU1w1vWGXRXsTu9D8MMyHx5q?location=recipe_home");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(36, "로제떡볶이", 0, writeDate, 40, "2인분", "★☆☆", null, 이미지, "한식", "기타", "https://wtable.co.kr/recipes/GUrB1ACmJruPLHQjX1t5oSt5?location=search");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(37, "아귀찜", 0, writeDate, 45, "3인분", "★★☆", null, 이미지, "한식", "기타", "https://wtable.co.kr/recipes/rwdwpF2m1kWorEj884viwBP4?location=search");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(38, "치즈닭갈비", 0, writeDate, 45, "3~4인분", "★★☆", null, 이미지, "한식", "기타", "https://wtable.co.kr/recipes/ekAHodzCtpMHFj9Y794SMvDz?location=search");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(39, "매운 순대볶음", 0, writeDate, 30, "2인분", "★★☆", null, 이미지, "한식", "기타", "https://wtable.co.kr/recipes/ix8KuNtH1kt5EbAGnZqkGfCY?location=search");
+insert into recipe (recipeId, recipeName, view, writeDate, runTime, portion, cookLevel, userId, recipeImg, majorCategory, subCategory, ref) 
+values(40, "식혜", 0, writeDate, 360, "3리터 분량", "★★☆", null, 이미지, "한식", "기타", "https://wtable.co.kr/recipes/ix8KuNtH1kt5EbAGnZqkGfCY?location=search");
